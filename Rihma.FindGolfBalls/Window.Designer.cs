5c8bc0e31696d2c8a429f3165b2f206d8388a16a
Marti Kaljuve <martik@gmail.com>
2010-09-29 23:12:07 +0300

    Algus pilditöötluses.

---
+++ Rihma.FindGolfBalls/Window.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Rihma.FindGolfBalls
+{
+	partial class Window
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.uxImage = new Emgu.CV.UI.ImageBox();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.uxFilename = new System.Windows.Forms.TextBox();
+			this.uxFilenameLabel = new System.Windows.Forms.Label();
+			this.uxFilenameBrowse = new System.Windows.Forms.Button();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.uxSidePanel = new System.Windows.Forms.TabControl();
+			this.uxSidePanelTab1 = new System.Windows.Forms.TabPage();
+			this.uxTable = new System.Windows.Forms.TableLayoutPanel();
+			this.uxCannyCheckbox = new System.Windows.Forms.CheckBox();
+			this.uxWebcam = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.uxImage)).BeginInit();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.uxSidePanel.SuspendLayout();
+			this.uxSidePanelTab1.SuspendLayout();
+			this.uxTable.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// uxImage
+			// 
+			this.uxImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayoutPanel1.SetColumnSpan(this.uxImage, 4);
+			this.uxImage.Location = new System.Drawing.Point(3, 32);
+			this.uxImage.Name = "uxImage";
+			this.uxImage.Size = new System.Drawing.Size(416, 340);
+			this.uxImage.TabIndex = 0;
+			this.uxImage.TabStop = false;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.AutoSize = true;
+			this.tableLayoutPanel1.ColumnCount = 4;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.Controls.Add(this.uxImage, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.uxFilename, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.uxFilenameLabel, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.uxFilenameBrowse, 2, 0);
+			this.tableLayoutPanel1.Controls.Add(this.uxWebcam, 3, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(422, 375);
+			this.tableLayoutPanel1.TabIndex = 1;
+			// 
+			// uxFilename
+			// 
+			this.uxFilename.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.uxFilename.Location = new System.Drawing.Point(35, 4);
+			this.uxFilename.Name = "uxFilename";
+			this.uxFilename.Size = new System.Drawing.Size(260, 20);
+			this.uxFilename.TabIndex = 2;
+			// 
+			// uxFilenameLabel
+			// 
+			this.uxFilenameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.uxFilenameLabel.AutoSize = true;
+			this.uxFilenameLabel.Location = new System.Drawing.Point(3, 8);
+			this.uxFilenameLabel.Name = "uxFilenameLabel";
+			this.uxFilenameLabel.Size = new System.Drawing.Size(26, 13);
+			this.uxFilenameLabel.TabIndex = 3;
+			this.uxFilenameLabel.Text = "File:";
+			// 
+			// uxFilenameBrowse
+			// 
+			this.uxFilenameBrowse.AutoSize = true;
+			this.uxFilenameBrowse.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.uxFilenameBrowse.Location = new System.Drawing.Point(301, 3);
+			this.uxFilenameBrowse.Name = "uxFilenameBrowse";
+			this.uxFilenameBrowse.Size = new System.Drawing.Size(52, 23);
+			this.uxFilenameBrowse.TabIndex = 4;
+			this.uxFilenameBrowse.Text = "&Browse";
+			this.uxFilenameBrowse.UseVisualStyleBackColor = true;
+			this.uxFilenameBrowse.Click += new System.EventHandler(this.uxFilenameBrowse_Click);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.uxSidePanel);
+			this.splitContainer1.Size = new System.Drawing.Size(716, 375);
+			this.splitContainer1.SplitterDistance = 422;
+			this.splitContainer1.TabIndex = 2;
+			// 
+			// uxSidePanel
+			// 
+			this.uxSidePanel.Controls.Add(this.uxSidePanelTab1);
+			this.uxSidePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.uxSidePanel.Location = new System.Drawing.Point(0, 0);
+			this.uxSidePanel.Name = "uxSidePanel";
+			this.uxSidePanel.SelectedIndex = 0;
+			this.uxSidePanel.Size = new System.Drawing.Size(290, 375);
+			this.uxSidePanel.TabIndex = 0;
+			// 
+			// uxSidePanelTab1
+			// 
+			this.uxSidePanelTab1.BackColor = System.Drawing.SystemColors.Control;
+			this.uxSidePanelTab1.Controls.Add(this.uxTable);
+			this.uxSidePanelTab1.Location = new System.Drawing.Point(4, 22);
+			this.uxSidePanelTab1.Name = "uxSidePanelTab1";
+			this.uxSidePanelTab1.Padding = new System.Windows.Forms.Padding(3);
+			this.uxSidePanelTab1.Size = new System.Drawing.Size(282, 349);
+			this.uxSidePanelTab1.TabIndex = 0;
+			this.uxSidePanelTab1.Text = "Threshold";
+			// 
+			// uxTable
+			// 
+			this.uxTable.ColumnCount = 3;
+			this.uxTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.uxTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.uxTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+			this.uxTable.Controls.Add(this.uxCannyCheckbox, 0, 0);
+			this.uxTable.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.uxTable.Location = new System.Drawing.Point(3, 3);
+			this.uxTable.Name = "uxTable";
+			this.uxTable.RowCount = 2;
+			this.uxTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+			this.uxTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.uxTable.Size = new System.Drawing.Size(276, 343);
+			this.uxTable.TabIndex = 1;
+			// 
+			// uxCannyCheckbox
+			// 
+			this.uxCannyCheckbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.uxCannyCheckbox.AutoSize = true;
+			this.uxCannyCheckbox.Checked = true;
+			this.uxCannyCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.uxCannyCheckbox.Enabled = false;
+			this.uxCannyCheckbox.Location = new System.Drawing.Point(3, 6);
+			this.uxCannyCheckbox.Name = "uxCannyCheckbox";
+			this.uxCannyCheckbox.Size = new System.Drawing.Size(111, 17);
+			this.uxCannyCheckbox.TabIndex = 0;
+			this.uxCannyCheckbox.Text = "Enabled";
+			this.uxCannyCheckbox.UseVisualStyleBackColor = true;
+			// 
+			// uxWebcam
+			// 
+			this.uxWebcam.AutoSize = true;
+			this.uxWebcam.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.uxWebcam.Location = new System.Drawing.Point(359, 3);
+			this.uxWebcam.Name = "uxWebcam";
+			this.uxWebcam.Size = new System.Drawing.Size(60, 23);
+			this.uxWebcam.TabIndex = 5;
+			this.uxWebcam.Text = "&Webcam";
+			this.uxWebcam.UseVisualStyleBackColor = true;
+			this.uxWebcam.Click += new System.EventHandler(this.uxWebcam_Click);
+			// 
+			// Window
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.ClientSize = new System.Drawing.Size(716, 375);
+			this.Controls.Add(this.splitContainer1);
+			this.MinimumSize = new System.Drawing.Size(200, 100);
+			this.Name = "Window";
+			this.Text = "Finding golf balls";
+			((System.ComponentModel.ISupportInitialize)(this.uxImage)).EndInit();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel1.PerformLayout();
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.ResumeLayout(false);
+			this.uxSidePanel.ResumeLayout(false);
+			this.uxSidePanelTab1.ResumeLayout(false);
+			this.uxTable.ResumeLayout(false);
+			this.uxTable.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private Emgu.CV.UI.ImageBox uxImage;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TextBox uxFilename;
+		private System.Windows.Forms.Label uxFilenameLabel;
+		private System.Windows.Forms.Button uxFilenameBrowse;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TabControl uxSidePanel;
+		private System.Windows.Forms.TabPage uxSidePanelTab1;
+		private System.Windows.Forms.CheckBox uxCannyCheckbox;
+		private System.Windows.Forms.TableLayoutPanel uxTable;
+		private System.Windows.Forms.Button uxWebcam;
+	}
+}
+
---
+++ Rihma.FindGolfBalls/Window.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace Rihma.FindGolfBalls
+{
+	public partial class Window : Form
+	{
+		private Capture _capture;
+		private Gray cannyThreshold = new Gray(180);
+		private Gray cannyThresholdLinking = new Gray(120);
+		private Gray circleAccumulatorThreshold = new Gray(120);
+		private Gray binaryThreshold = new Gray(100);
+		private Gray binaryMaximumValue = new Gray(255);
+
+		public Window()
+		{
+			InitializeComponent();
+			InitializeControls();
+
+			const double twoPi = 2 * Math.PI;
+			const string filename = "C:\\Downloads\\capture2-18.avi";
+			_capture = new Capture(filename);
+			uxFilename.Text = filename;
+
+			var timer = new Stopwatch();
+			var timerCount = 0;
+			long timeLast = 0;
+			long timeNow = 0;
+			long timeElapsed = 0;
+			float fps = 0;
+
+			timer.Start();
+			var font = new MCvFont(FONT.CV_FONT_HERSHEY_COMPLEX, 1.0, 1.0);
+			var smallFont = new MCvFont(FONT.CV_FONT_HERSHEY_COMPLEX_SMALL, 1.0, 1.0);
+
+			uxImage.Size = new Size(_capture.Width, _capture.Height);
+
+			Application.Idle +=
+				(sender, args) =>
+				{
+					// Loop video files
+					if (_capture.GetCaptureProperty(CAP_PROP.CV_CAP_PROP_POS_AVI_RATIO) >= 1) {
+						_capture = new Capture();
+						return;
+					}
+
+					if (timer.IsRunning)
+					{
+						timerCount++;
+						timeNow = timer.ElapsedMilliseconds;
+						long timeDelta = timeNow - timeLast;
+						timeElapsed += timeDelta;
+						timeLast = timeNow;
+
+						if (timeElapsed >= 1000)
+						{
+							fps = (float) timerCount/(timeElapsed/1000);
+							timerCount = 0;
+							timeElapsed = 0;
+						}
+					}
+
+					var img = _capture.QueryFrame();
+
+					if (img == null) return;
+
+					var gray = img.Convert<Gray, byte>().PyrDown().PyrUp();
+					//gray._Dilate(2);
+					//gray._Erode(2);
+					//gray._EqualizeHist();
+					gray._ThresholdBinary(binaryThreshold, binaryMaximumValue);
+					gray = gray.Canny(cannyThreshold, cannyThresholdLinking);
+
+					using (var storage = new MemStorage())
+					{
+						var contours = gray.FindContours(CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_NONE, RETR_TYPE.CV_RETR_EXTERNAL, storage);
+
+						for (; contours != null; contours = contours.HNext)
+						{
+							var moments = contours.GetMoments();
+
+							//var eccentricity = GetEccentricity(moments);
+
+							var roundness = (contours.Perimeter * contours.Perimeter) / (twoPi * contours.Area);
+							//if (roundness > 20) continue;
+
+							var cross = new Cross2DF(new PointF((float)moments.GravityCenter.x, (float)moments.GravityCenter.y), 10, 10);
+							//gray.Draw(cross, new Gray(100), 2);
+							img.Draw(cross, new Bgr(Color.Brown), 2);
+
+							img.Draw(contours.BoundingRectangle, new Bgr(Color.Blue), 1);
+							img.Draw(string.Format("{0:0.0}", roundness), ref smallFont, contours.BoundingRectangle.Location, new Bgr(Color.WhiteSmoke));
+
+							//gray.Draw(contours.BoundingRectangle, new Gray(70), 3);
+							//gray.DrawPolyline(contours.ToArray(), false, new Gray(35), 2);
+							//img.DrawPolyline(contours.ToArray(), false, new Bgr(Color.Gray), 2);
+						}
+					}
+
+					//gray.Draw(string.Format("FPS: {0}", fps), ref font, new Point(10, 30), new Gray(255));
+					img.Draw(string.Format("FPS: {0}", fps), ref font, new Point(10, 30), new Bgr(Color.White));
+
+					//uxImage.Image = gray;
+					uxImage.Image = img;
+				};
+		}
+
+		//private static double GetEccentricity(MCvMoments moments) {
+		//    var m1 = moments.GetCentralMoment(2, 0);
+		//    var m2 = moments.GetCentralMoment(0, 2);
+		//    var m11 = m1 - m2;
+		//    var m22 = m1 + m2;
+		//    var m3 = moments.GetCentralMoment(1, 1);
+		//    return ((m11*m11) - 4*(m3*m3)) / (m22 * m22);
+		//}
+
+		private void InitializeControls()
+		{
+			uxTable.SuspendLayout();
+			AddSlider("Binary Threshold", this, "BinaryThreshold", 0, 255);
+			AddSlider("Binary Maximum Value", this, "BinaryMaximumValue", 0, 255);
+
+			AddSlider("Threshold", this, "CannyThreshold", 0, 255);
+			AddSlider("Threshold Linking", this, "CannyThresholdLinking", 0, 255);
+			AddSlider("Circle Accumulator Threshold", this, "CannyCircleAccumulatorThreshold", 0, 255);
+			uxTable.ResumeLayout(true);
+		}
+
+		private void AddSlider(string text, object dataSource, string dataMember, int min, int max)
+		{
+			int row = uxTable.RowCount++ - 1;
+			uxTable.RowStyles.Insert(row, new RowStyle(SizeType.Absolute, 30));
+
+			var label = new Label();
+			label.Text = text;
+			label.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top;
+
+			var slider = new TrackBar();
+			slider.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top;
+			slider.Minimum = min;
+			slider.Maximum = max;
+			slider.DataBindings.Add("Value", dataSource, dataMember, true, DataSourceUpdateMode.OnPropertyChanged);
+
+			var textbox = new TextBox();
+			textbox.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top;
+			textbox.DataBindings.Add("Text", dataSource, dataMember, true, DataSourceUpdateMode.OnPropertyChanged);
+
+			uxTable.Controls.Add(label, 0, row);
+			uxTable.Controls.Add(slider, 1, row);
+			uxTable.Controls.Add(textbox, 2, row);
+		}
+
+		private void uxFilenameBrowse_Click(object sender, EventArgs e)
+		{
+			var dialog =
+				new OpenFileDialog
+				{
+					DefaultExt = "avi",
+					Filter = "Video files (*.avi)|*.avi|All files (*.*)|*.*"
+				};
+
+			var result = dialog.ShowDialog(this);
+			if (result != DialogResult.OK) return;
+
+			uxFilename.Text = dialog.FileName;
+			_capture = new Capture(dialog.FileName);
+		}
+
+		private void uxWebcam_Click(object sender, EventArgs e)
+		{
+			uxFilename.Clear();
+			_capture = new Capture();
+		}
+
+		public int BinaryThreshold
+		{
+			get { return (int) binaryThreshold.Intensity; }
+			set { binaryThreshold = new Gray(value); }
+		}
+
+		public int BinaryMaximumValue
+		{
+			get { return (int)binaryMaximumValue.Intensity; }
+			set { binaryMaximumValue = new Gray(value); }
+		}
+
+		public decimal CannyThreshold
+		{
+			get { return (decimal)cannyThreshold.Intensity; }
+			set { cannyThreshold = new Gray((double)value); }
+		}
+
+		public decimal CannyThresholdLinking
+		{
+			get { return (decimal)cannyThresholdLinking.Intensity; }
+			set { cannyThresholdLinking = new Gray((double)value); }
+		}
+
+		public decimal CannyCircleAccumulatorThreshold
+		{
+			get { return (decimal)circleAccumulatorThreshold.Intensity; }
+			set { circleAccumulatorThreshold = new Gray((double)value); }
+		}
+	}
+}
